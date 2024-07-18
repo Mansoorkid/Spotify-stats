b83685ddfea64a7552e46a7d8851335ace285c38
Mansoorkid <shaunmansoor2@gmail.com>
2024-07-18 16:50:12 -0700

    initial working file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from auth import get_spotify_client
-from stats import display_top_tracks, display_top_artists
+from stats import display_top_tracks, display_top_artists, display_menu
 from dotenv import load_dotenv
 import os
 
@@ -25,6 +25,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    display_top_tracks(sp)
-    display_top_artists(sp)
